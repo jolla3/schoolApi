104ef5d8539b7e2f4682420d7dca7ae61af1d4b3
joll3a <its.jolla6@gmail.com>
2025-08-04 09:56:38 +0300

    jwt

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -58,7 +58,7 @@ exports.login= async(req,res)=>{
     const token= jwt.sign({
      userId:user._id,
      role:user.role,},
-     process.env.JWt_SECRET,{
+     process.env.JWT_SECRET,{
      expiresIn:"7d"}
     )
     res.json({message:"login succesfully",
