eb3aca5eee8c1bc926c96c2137599856adc42ab0
joll3a <its.jolla6@gmail.com>
2025-08-07 12:23:05 +0300

    v9.5

--- controllers/parentDashController.js
+++ controllers/parentDashController.js
@@ -1,42 +1,90 @@
-const {User,Parent,Classroom, Student, Assignment}= require('../models/schoolDb')
+const {Parent,User}=require('../model/SchoolDb')
+const bcrypt = require('bcrypt')
 
-
-
-// get the chilren belonging to the logged in parernt
-exports.parentDash= async(req,res)=>{
+// Add Parent 
+exports.addParent=async (req,res) => {
     try {
-        // get loggeeeeeeed in user to link with the parent
-        const userId= req.user.userId
-        // find the parent using the user userId
-        const user = await User.findById(userId)
-        .populate("parent");
-        console.log(user);
-        const parent= user.parent
-        // find the children belonging to the parent
-        const children= await Student.find({parent:parent._id})
-        .populate('classroom')
-        console.log("children",children);
+        // destructure variable to check if the parent exist 
+        const {email,nationalId, name}=req.body
+        // check using email
+        const existingParentEmail=await User.findOne({email})
+        if(existingParentEmail) return res.json({message:"Email already taken"})
+        // check using the id 
+        const existingParentId=await Parent.findOne({nationalId})
+        console.log("nationalId", nationalId)
+        if(existingParentId) return res.json({message:"National Id has already been registered"})
+        // when all check are good we now save the new parent
+    
+        const newParent =new Parent(req.body)
+        const savedParent= await newParent.save()
         
-        res.status(200).json({parent,children})
-        // res.status(200).json(parent)
+        // creating the parent user account
+        const defaultPassword='parent1234'
+        const hashedPassword=await bcrypt.hash(defaultPassword,10)
+        console.log(hashedPassword)
 
+        const newUser = new User({
+            name,email,
+            password:hashedPassword,
+            role:"parent",
+            parent:savedParent._id
+        })
+        await newUser.save()
+        console.log(newUser)
+        res.status(201).json({parent:savedParent,newUser,message:"Parent ad user account created successfully"})
         
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(500).json({message:error.message})
     }
+    
 }
 
-
-// get the students Assigment
-exports.getClassAssigments= async(req,res)=>{
+// get all parents
+exports.getAppParents= async (req,res) => {
     try {
-        const assigments = await Assigment.find({
-            classroom:req.params.id})
-            .populate('postedBy')
-            .sort({dueDate:1})
-            res.json(assigment)
-        
+        const parents=await Parent.find()
+        res.json(parents)
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(500).json({message:error.message})
+    }
+    
+}
+
+// update parent by admin
+exports.updateParent=async (req,res) => {
+    try {
+        const updatedParent= await Parent.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new:true}
+        )
+        if(!updatedParent) return res.status(404).json({message:"Parent is not found"})
+        res.status(201).json(updatedParent)
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }        
+}
+
+exports.deleteParent= async (req,res) => {
+    try {
+        const deletedParent=await Parent.findByIdAndDelete(req.params.id)
+        if(!deletedParent) return res.status(404).json({message:"Parent Not Found"})
+        // delete also the asociated user account
+
+        await User.findOneAndDelete({parent:req.params.id})
+        res.status(200).json({message: "Parent account deleted successfully"})
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+    
+}
+
+exports.getParentById=async (params) => {
+    try {
+        const parent=await Parent.findOne({nationalId:req.params.id})
+        if(!parent) return res.json({message:'Parent Not Found'})
+            res.json(parent)
+    } catch (error) {
+        res.status(500).json({message:error.message})
     }
 }
--- routes/parentRouter.js
+++ routes/parentRouter.js
@@ -1,15 +1,13 @@
 const express= require('express')
-const router= express.Router()
-const parentController= require('../controllers/parentController')
-
+const router=express.Router()
+const parentController= require('../controller/parentController')
 // authorization
-const{auth,authorizeRoles}= require('../middleware/auth')
+const {auth,authorizeRoles}=require('../middleware/auth')
 
-router.post('/',auth,authorizeRoles('admin'), parentController.addParent)
-router.get('/all', auth,authorizeRoles('admin'), parentController.getAllParents)
-// router.get('/:id', classroomController.getAllClassroomById)
-router.put('/:id',auth,authorizeRoles("admin"), parentController.updateParent)
-router.delete('/:id',auth,authorizeRoles("admin"), parentController.deleteParent)
+router.post("/",auth,authorizeRoles("admin"),parentController.addParent)
+router.get('/',auth,parentController.getAppParents)
+router.get('/:id',auth,parentController.getParentById)
+router.put('/:id',auth, authorizeRoles('admin'),parentController.updateParent)
+router.delete('/:id',auth,authorizeRoles('admin'),parentController.deleteParent)
 
-
-module.exports = router
+module.exports=router
